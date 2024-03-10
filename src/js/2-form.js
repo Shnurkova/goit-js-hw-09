b2ba791e865244a1331c7a5bf9cf97c46120aab6
Shnurkova <Annshnur@gmail.com>
2024-03-10 18:42:25 +0200

    add

---
+++ src/js/2-form.js
@@ -0,0 +1,10 @@
+const formUl = document.querySelector(".feedback-form");
+const textarea = document.querySelector("textarea");
+
+
+// formUl.addEventListener("input", formSubmitHandler)
+
+function formSubmitHandler(event) {
+    const text = textarea.value;
+    const email = 
+}
