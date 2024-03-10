1cd54eae9db95f918a307f293f953cd746eb3fdd
Shnurkova <Annshnur@gmail.com>
2024-03-10 20:40:22 +0200

    add

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,9 +1,26 @@
-const formUl = document.querySelector(".feedback-form");
+const STORAGE_KEY = "feedback-form-state"
+
+const form = document.querySelector(".feedback-form");
 const textarea = document.querySelector("textarea");
 
-
-// formUl.addEventListener("input", formSubmitHandler)
-
 function formSubmitHandler(event) {
+    event.preventDefault();
+
+    const email = form.elements.email.value;
     const text = textarea.value;
+    const data = JSON.stringify({ email, text });
+    console.log(data);
+    localStorage.setItem(STORAGE_KEY, data);
 }
+
+form.addEventListener('submit', formSubmitHandler);
+
+const jsn = localStorage.getItem(STORAGE_KEY) ?? "";
+try {
+const data = JSON.parse(jsn);
+console.log(data);
+textarea.value = data.text;
+form.elements.email.value = data.email;
+} catch {
+    console.log("No save data!")
+};
