dc03f6359fe689802ce9614e7a1c8f5a5bbe15ab
Shnurkova <Annshnur@gmail.com>
2024-03-10 14:46:59 +0200

    add

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -67,6 +67,11 @@ const images = [
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+    
+});
+
 console.log(SimpleLightbox);
 const galleryUl = document.querySelector(".gallery");
 
