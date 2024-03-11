c76858395aea363495e12d5ff0fbe3d97eea567a
Shnurkova <Annshnur@gmail.com>
2024-03-11 16:47:25 +0200

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,35 +1,35 @@
 const STORAGE_KEY = "feedback-form-state"
 
 const form = document.querySelector(".feedback-form");
-const input = document.querySelector("input");
-const textarea = document.querySelector("textarea");
+const emailInput = document.querySelector("input");
+const messageTextarea = document.querySelector("textarea");
 
 function formSubmitHandler(event) {
     event.preventDefault();
     
-    if (input.value.trim() === "" || textarea.value.trim() === "") {
+    if (emailInput.value.trim() === "" || messageTextarea.value.trim() === "") {
         return alert(`All form fields must be filled in`);
     } else {
         localStorage.removeItem(STORAGE_KEY);
     }
 
-    const email = input.value.trim();
-    const text = textarea.value.trim();
+    const email = emailInput.value.trim();
+    const text = messageTextarea.value.trim();
     const data = JSON.stringify({ email, text });
-    console.log(data);
+    
     localStorage.setItem(STORAGE_KEY, data);
+
+    const jsn = localStorage.getItem(STORAGE_KEY) ?? "";
+    try {
+        const savedData = JSON.parse(jsn);
+        console.log(savedData);
+        messageTextarea.value = savedData.text;
+        emailInput.value = savedData.email;
+    } catch {
+        console.log("No saved data!");
+    };
 
     event.currentTarget.reset();
 }
 
 form.addEventListener('submit', formSubmitHandler);
-
-const jsn = localStorage.getItem(STORAGE_KEY) ?? "";
-try {
-const data = JSON.parse(jsn);
-console.log(data);
-textarea.value = data.text;
-form.elements.email.value = data.email;
-} catch {
-    console.log("No save data!")
-};
