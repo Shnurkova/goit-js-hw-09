9dc5b4165cc22e957c8642c0918e1cf7e4f6380a
Shnurkova <Annshnur@gmail.com>
2024-03-11 19:06:34 +0200

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,40 +1,39 @@
 const STORAGE_KEY = "feedback-form-state"
-
 const form = document.querySelector(".feedback-form");
 const emailInput = document.querySelector("input");
 const messageTextarea = document.querySelector("textarea");
+const email = emailInput.value.trim();
+const text = messageTextarea.value.trim();
+const data = JSON.stringify({ email, text });
+let formData = {};
+updateForm();
 
+function onInputForm(evt) {
+    formData[evt.target.name] = evt.target.value.trim(); 
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData)); 
+}
+  
+form.addEventListener('input', onInputForm);
+  
 function formSubmitHandler(event) {
     event.preventDefault();
-    
     if (emailInput.value.trim() === "" || messageTextarea.value.trim() === "") {
         return alert(`All form fields must be filled in`);
     } else {
         localStorage.removeItem(STORAGE_KEY);
+        console.log(formData);
+        formData = {};
+        event.currentTarget.reset();
     }
-
-    const email = emailInput.value.trim();
-    const text = messageTextarea.value.trim();
-    const data = JSON.stringify({ email, text });
-    
-    localStorage.setItem(STORAGE_KEY, data);
-
-    const jsn = localStorage.getItem(STORAGE_KEY) ?? "";
-    try {
-        const savedData = JSON.parse(jsn);
-        console.log(savedData);
-        messageTextarea.value = savedData.text;
-        emailInput.value = savedData.email;
-    } catch {
-        console.log("No saved data!");
-    };
-
-    event.currentTarget.reset();
 }
 
-window.addEventListener('beforeunload', function(event) {
-    event.preventDefault();
-    localStorage.getItem(STORAGE_KEY);
-});
-
 form.addEventListener('submit', formSubmitHandler);
+
+function updateForm() {
+    let savedFormData = localStorage.getItem(STORAGE_KEY);
+    if (savedFormData) {
+      formData = JSON.parse(savedFormData) || {};
+      emailInput.value = formData.email || '';
+      messageTextarea.value = formData.message || '';
+    }
+  }
